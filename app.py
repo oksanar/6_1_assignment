1afcafdf0dbd7b99ee89d08b50401b8e9a1bf1dd
oksanar <oksanaregunova@gmail.com>
2024-02-20 00:04:06 +0200

    Add calculator app with add and subtract two numbers functionality

---
+++ app.py
@@ -0,0 +1,38 @@
+# This function adds two numbers
+def add(x, y):
+    return x + y
+
+# This function subtracts two numbers
+def subtract(x, y):
+    return x - y
+
+print("Select operation.")
+print("1.Add")
+print("2.Subtract")
+
+while True:
+    # take input from the user
+    choice = input("Enter choice(1/2): ")
+
+    # check if choice is one of the four options
+    if choice in ('1', '2'):
+        try:
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+            continue
+
+        if choice == '1':
+            print(num1, "+", num2, "=", add(num1, num2))
+
+        elif choice == '2':
+            print(num1, "-", num2, "=", subtract(num1, num2))
+
+        # check if user wants another calculation
+        # break the while loop if answer is no
+        next_calculation = input("Let's do next calculation? (yes/no): ")
+        if next_calculation == "no":
+          break
+    else:
+        print("Invalid Input")
